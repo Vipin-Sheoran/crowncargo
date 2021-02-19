1bd2476cffa309a2f6df14532badc41e8038443b
vipin sheoran <sheoranvipin2910@gmail.com>
2021-02-19 12:56:06 +0530

    Updates

---
+++ precache-manifest.7cf64a114685f9b6022198e6a95c5a88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3f0a74c150ff87d4f23ecc3364750e",
+    "url": "/fasttrack/index.html"
+  },
+  {
+    "revision": "bc95276590c6ca1fed7b",
+    "url": "/fasttrack/static/css/main.de034a0a.chunk.css"
+  },
+  {
+    "revision": "feecea98231489c5cd6d",
+    "url": "/fasttrack/static/js/2.a1d27a0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/fasttrack/static/js/2.a1d27a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc95276590c6ca1fed7b",
+    "url": "/fasttrack/static/js/main.2c8134c0.chunk.js"
+  },
+  {
+    "revision": "65603fc6d2ebcf14c085",
+    "url": "/fasttrack/static/js/runtime-main.94db7d8f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b9ac23ec68d966e954c42bf951e7d8d.js"
+  "/fasttrack/precache-manifest.7cf64a114685f9b6022198e6a95c5a88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fasttrack/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
