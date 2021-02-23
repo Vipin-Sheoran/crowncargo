c5240d66ac30c747855c8b5762096c7f4e01724c
vipin sheoran <sheoranvipin2910@gmail.com>
2021-02-23 22:17:45 +0530

    Updates

---
+++ precache-manifest.dbad1199d7121a597486890b625313e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050afe4dba8cc352f5180d5b427cf131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bef3a944739be34bd759",
+    "url": "/static/css/main.0919d65b.chunk.css"
+  },
+  {
+    "revision": "feecea98231489c5cd6d",
+    "url": "/static/js/2.a1d27a0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a1d27a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef3a944739be34bd759",
+    "url": "/static/js/main.f4e0a2f3.chunk.js"
+  },
+  {
+    "revision": "e22fd322f0b45aec7fb4",
+    "url": "/static/js/runtime-main.a7f6340d.js"
+  }
+]);
